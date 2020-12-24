c937ac8f8fdff2fd3fe976c4e468b2d404c85c65
rachit2525 <rawasthi2525@gmail.com>
2020-12-25 03:41:33 +0530

    working PLAY and STOP

---
+++ discord bots/main.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '-';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log('testBot is online!');
+});
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping') {
+        client.commands.get('ping').execute(message, args);
+    } else if(command === 'play') {
+        client.commands.get('play').execute(message, args);
+    } else if(command === 'stop') {
+        client.commands.get('leave').execute(message, args);
+    }
+
+
+})
+
+client.login('NzkxNDA1MTIwNzYzMDY4NDE2.X-OrdA.sAspUktzEkdI2rVWwrvXv_Ts6MI');
